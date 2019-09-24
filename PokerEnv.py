2b7215cae01e774ad040cb96756099756d553924
Surender Harsha <battusurender96@gmail.com>
2019-09-24 21:51:50 +0200

    Add files via upload

---
+++ PokerEnv.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 21:37:28 2019
+
+@author: battu
+"""
+
+from PokerGame import *
+
+
+#OpenAI Gym Environment for Poker
+class PokerEnvironment(object):
+    
+    
+    def __init__(self):
+        pass
+    
+    def reset(self):
+        pass
+    
+    def step(self):
+        pass
+    
+    def render(self):
+        pass
+        # We show the flask or html game here, when it is implemented
+        
