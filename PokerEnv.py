e8d01b62eb6aa366d070cb6802f71f80fb754fa2
Surender Harsha <battusurender96@gmail.com>
2019-09-26 14:44:49 +0200

    Add files via upload

--- PokerEnv.py
+++ PokerEnv.py
@@ -7,7 +7,7 @@ Created on Tue Sep 24 21:37:28 2019
 
 from PokerGame import *
 
-
+action_dictionary = {'pre_flop':['CALL','RAISE','FOLD'],'flop':[]}
 #OpenAI Gym Environment for Poker
 class PokerEnvironment(object):
     
@@ -27,7 +27,11 @@ class PokerEnvironment(object):
         
         self.main_deck = self.deck_object.return_deck()
         
-        
+        self.stage = None
+        self.minimum_bet_amount = 10
+        self.bets_placed = []
+        self.pointer = 0
+        self.folden_indexes = []
         pass
     
     def reset(self):
@@ -48,7 +52,91 @@ class PokerEnvironment(object):
         # We show the flask or html game here, when it is implemented
         #Send all information as json to frontend code, where it is rendered.
         
+    def set_stage(self):
+        for i in range(self.n_players):
+            self.player_objects[i].set_stage(self.stage)
+            
+    def del_folden_index(self,a,b,index):
+            del a[index]
+            try:
+                del b[index]
+            except Exceptions as e:
+                pass
+            return a,b
+    def get_player_actions(self):
+        
+        action_list = action_dictionary[self.stage]
+        
+        for i in range(self.n_players):
+            action = self.player_objects[i].make_decision(self.bets_placed,i,None)
+            
+            action_str = action_list[action]
+            if self.stage == 'pre_flop':
+                
+                if action_str == 'CALL':
+                    
+                    if len(self.bets_placed)>0:
+                        
+                        bet_amount  = self.minimum_bet_amount
+                        self.pointer = i
+                        try:
+                            self.bets_placed[i] = bet_amount
+                        except Exception as e:
+                            self.bets_placed.append(bet_amount)
+                        self.player_objects[i].current_coins -= bet_amount
+                        self.minimum_bet_amount = bet_amount
+                        
+                    else:
+                        self.bets_placed.append(self.minimum_bet_amount)
+                        self.player_objects[i].current_coins -= self.minimum_bet_amount
+                if action_str == "RAISE":
+                    if len(self.bets_placed)>0:
+                        bet_amount = self.minimum_bet_amount*2
+                        self.pointer = i
+                        try:
+                            self.bets_placed[i] = bet_amount
+                        except Exception as e:
+                            self.bets_placed.append(bet_amount)
+                        self.player_objects[i].current_coins -= bet_amount
+                        self.minimum_bet_amount = bet_amount
+                        
+                    else:
+                        self.bets_placed.append(self.minimum_bet_amount*2)
+                        self.minimum_bet_amount *= 2
+                        self.player_objects[i].current_coins -= self.minimum_bet_amount
+                
+                if action_str == "FOLD":
+                    self.folden_indexes.append(i)
+                    #del self.player_objects[i]
+                    
+        self.n_players -= len(self.folden_indexes)
+        
+            
+        for j in range(len(self.folden_indexes)):
+            self.player_objects,self.bets_placed = self.del_folden_index(self.player_objects,self.bets_placed,self.folden_indexes[j])
+            
+            self.folden_indexes = [x-1 for x in self.folden_indexes]
+        self.folden_indexes=[]
+        maximum = max(self.bets_placed)
+        temp = self.bets_placed
+        temp  = [x - maximum for x in temp]
+        if sum(temp) != 0:
+            self.get_player_actions()
+            
+                        
+                    
+                        
+                        
+                    
+        
+        
     def pre_flop(self):
+        
+        self.stage = 'pre_flop'
+        self.set_stage()
+        self.get_player_actions()
+        
+        
         
         
         
@@ -79,11 +167,25 @@ class Agent(object):
         self.current_coins = coins
         self.player = player_object
         self.hand = None
+        self.stage = None
         self.P_rp=0
-    
-    
-    
         
+        
+        self.available_actions = None
+        
+    def set_stage(self,stage):
+        self.stage = stage
+        self.available_actions = action_dictionary[self.stage]
+        
+    def make_decision(self, bets_placed,player_id,community_cards):
+        print("Bets Placed")
+        print(bets_placed,self.current_coins)
+        print("Player ID:",player_id)
+        a = input("Input for player:")
+        return int(a)
+        pass
+    
+    
     def calculate_probability_of_hand(self,hand):
         self.P_rp = self.P_RoyalFlush(hand)
         
@@ -122,7 +224,7 @@ for i in range(8):
 env = PokerEnvironment(players_list)
 env.reset()
 env.show()
-
+env.pre_flop()
 
 
 
